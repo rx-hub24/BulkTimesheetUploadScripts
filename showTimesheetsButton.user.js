0416efa00f304c4cd0b99cd33a158854cef5cb51
Emma Nipperess <emmanipperess@hub24.com.au>
2024-03-12 12:26:15 +1000

    Renames var

--- showTimesheetsButton.user.js
+++ showTimesheetsButton.user.js
@@ -11,28 +11,28 @@
 (function() {
     'use strict';
   // Create a new button element
-    var googleButton = document.createElement('button');
-    googleButton.innerHTML = "Go to Timesheets"; // Set the text inside the button
+    var openTimesheetsBtn = document.createElement('button');
+    openTimesheetsBtn.innerHTML = "Go to Timesheets"; // Set the text inside the button
     // Style the button
-    googleButton.style.position = 'fixed';
-    googleButton.style.top = '10px'; // Adjusted to not be exactly at the edge
-    googleButton.style.left = '50%'; // Center the button horizontally
-    googleButton.style.transform = 'translateX(-50%)'; // Adjust for exact centering
-    googleButton.style.backgroundColor = '#4CAF50'; // A pleasant green
-    googleButton.style.color = 'white';
-    googleButton.style.padding = '10px 20px';
-    googleButton.style.zIndex = '10000'; // Ensure it's on top of other content
-    googleButton.style.border = 'none';
-    googleButton.style.borderRadius = '5px';
-    googleButton.style.cursor = 'pointer'; // Change cursor on hover
-    googleButton.style.fontSize = '16px';
-    googleButton.style.fontFamily = 'Arial, sans-serif';
+    openTimesheetsBtn.style.position = 'fixed';
+    openTimesheetsBtn.style.top = '10px'; // Adjusted to not be exactly at the edge
+    openTimesheetsBtn.style.left = '50%'; // Center the button horizontally
+    openTimesheetsBtn.style.transform = 'translateX(-50%)'; // Adjust for exact centering
+    openTimesheetsBtn.style.backgroundColor = '#4CAF50'; // A pleasant green
+    openTimesheetsBtn.style.color = 'white';
+    openTimesheetsBtn.style.padding = '10px 20px';
+    openTimesheetsBtn.style.zIndex = '10000'; // Ensure it's on top of other content
+    openTimesheetsBtn.style.border = 'none';
+    openTimesheetsBtn.style.borderRadius = '5px';
+    openTimesheetsBtn.style.cursor = 'pointer'; // Change cursor on hover
+    openTimesheetsBtn.style.fontSize = '16px';
+    openTimesheetsBtn.style.fontFamily = 'Arial, sans-serif';
 
     // Add click event listener to the button
-    googleButton.addEventListener('click', function() {
+    openTimesheetsBtn.addEventListener('click', function() {
         window.location.href = document.querySelector('[data-testid="elmo-menu-timesheet"]').href;
     });
 
     // Append the button to the body
-    document.body.appendChild(googleButton);
+    document.body.appendChild(openTimesheetsBtn);
 })();
